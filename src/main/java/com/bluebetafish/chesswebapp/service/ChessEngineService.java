322f68fe170d1824525705ae6bda43f8e33e796a
sourashisUbuntu <sourashiswtf@gmail.com>
2024-03-13 21:31:10 +0530

    added new game button and fixed problem of simultaneous requests reseting the engine(creating one engine instance per request now)
    Signed-off-by: sourashisUbuntu <sourashiswtf@gmail.com>

--- src/main/java/com/bluebetafish/chesswebapp/service/ChessEngineService.java
+++ src/main/java/com/bluebetafish/chesswebapp/service/ChessEngineService.java
@@ -3,20 +3,49 @@ package com.bluebetafish.chesswebapp.service;
 import net.andreinc.neatchess.client.UCI;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.time.LocalTime;
 
 @Service
 public class ChessEngineService {
 //    @Autowired
 //    private UCI stockfishUciWrapper;
 
-    @Autowired
-    private UCI bluebetafishUciWrapper;
+//    @Autowired
+//    private UCI bluebetafishUciWrapper;
 
     public String getBestMove(String fen) {
+
+        /*
+        * TODO:
+        * for the current version of engine, if it is calculating for some position (obtained from some request r1)
+        * and then some new request r2 comes and asks the engine to get best move for another position
+        * then 'ucinewgame' command will be executed and we wont get the result for request r1
+        *
+        * So for each best move request we have to create a new engine instance.
+        * We cant keep a list of engine instances and use them in round robin because of this reason.
+        *
+        * So for now, for each request, i am creating a new engine instance
+        * and destroying it once the request is processed
+        * */
+        UCI bluebetafishUciWrapper = new UCI();
+
+        System.out.println("\nReceived request: fen= " + fen);
+        System.out.println("Time= " + LocalTime.now());
+
+        bluebetafishUciWrapper.startBluebetafish();
         bluebetafishUciWrapper.uciNewGame();
         bluebetafishUciWrapper.positionFen(fen);
-        var result10depth = bluebetafishUciWrapper.bestMove(1500L, 60000).getResultOrThrow();
+
+        //* be careful: if you pass int as first arg, then overloaded function bestMove(depth, timeout) is called,
+        //* and if you pass long as first arg, then overloaded function bestMove(moveTime, timeout) is called
+        var result10depth = bluebetafishUciWrapper.bestMove(10000L, 60000).getResultOrThrow();
+
         System.out.println("Best move after analysing 10 moves deep: " + result10depth.getCurrent());
+        System.out.println("Time= " + LocalTime.now());
+
+        //* destroy the instance(TODO: keep this in finally block in case of exceptions)
+        bluebetafishUciWrapper.close();
+
 
         return result10depth.getCurrent();
     }
--- src/main/resources/static/static/js/ui-script/uiscript.js
+++ src/main/resources/static/static/js/ui-script/uiscript.js
@@ -1,18 +1,35 @@
 console.log("\nhello")
-// NOTE: this example uses the chess.js library:
-// https://github.com/jhlywa/chess.js
 
-var board = null
-var game = new Chess()
-var $status = $('#status')
-var $fen = $('#fen')
-var $pgn = $('#pgn')
+let board = null
+let statusDiv = document.getElementById('statusDiv')
+let fen = document.getElementById('fen')
+let pgn = document.getElementById('pgn')
+let game = new Chess()
 
+let playEngineMoveGetRequestXHR;
+
+
+/*
+* Documentattion:
+* -----------------------------------------
+* Fires when a piece is picked up.
+* The first argument to the function is the source of the piece, the second argument is the piece,
+* the third argument is the current position on the board, and the fourth argument is the current orientation.
+* The drag action is prevented if the function returns false. 
+*
+* Example:
+* - source (algebraic notation) -> examples: a1, c2, g5
+* - piece (some weird notation {'w' or 'b'} {pieceType in capital}) 
+*        -> example: bP(black pawn), wN(white knight), wK(white king),...
+* - position -> fen
+* - orientation -> white or black (from whose pov we are seeing the board)
+* 
+*/
 function onDragStartHandler (source, piece, position, orientation) {
-  // do not pick up pieces if the game is over
+  //* do not let the user drag any piece, if game is over
   if (game.game_over()) return false
 
-  // only pick up pieces for the side to move
+  //* only let user pick up pieces for the side to move
   if ((game.turn() === 'w' && piece.search(/^b/) !== -1) ||
       (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
     return false
@@ -20,48 +37,78 @@ function onDragStartHandler (source, piece, position, orientation) {
 }
 
 
-
+	
+/*
+* Fires when a piece is dropped.
+* The first argument to the function is the source of the dragged piece, 
+* the second argument is the target of the dragged piece, the third argument is the piece, 
+* the fourth argument is the new position once the piece drops, 
+* the fifth argument is the old position before the piece was picked up, 
+* and the sixth argument is the current orientation.
+* 
+* If 'snapback' is returned from the function, the piece will return to it's source square.
+* 
+* If 'trash' is returned from the function, the piece will be removed
+*
+* Example: 
+* - source, target: d2, d4, e1, g4
+* - Piece: wP, bK, bQ
+* - newPosition, oldPosition(fen string): rnbqkbnr/pppppppp/8/8/3P4/8/PPP1PPPP/RNBQKBNR
+* - Orientation: white, black
+*
+*/
 function onDropHandler (source, target) {  
-  // see if the move is legal
+  //* see if the move is legal
   var move = game.move({
     from: source,
     to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
+    promotion: 'q' // TODO: for now always promote to a queen for example simplicity
   })
 
-  // illegal move
-  if (move === null) return 'snapback'
+  //* for illegal move, return 'snapback' -> this will snap the piece back
+  //* to the original square from where drag started
+  if (move === null) 
+      return 'snapback'
 
-  // call the API
-//  $.post('/makemove', {'fen' : game.fen()}, function(data) {
-//    console.log(data)
-//
-//    // update game fen
-//    game.load(data.fen)
-//
-//    // update ui board
-//    board.position(data.fen)
-//  })
-
-    $.get("/api/bestmove?fen=" + game.fen(), function(response) {
-        console.log(response)
-        console.log("\ngame.fen(): " + game.fen())
-        if (game.move({from: response[0] + response[1], to: response[2] + response[3]}) === null) {
-            console.log(".move() failed!!!!")
-        }
-        console.log("game.fen(): " + game.fen())
-        // update game fen
-//        game.load(data.fen)
-
-        // update ui board
-        board.position(game.fen())
-    }).fail(function(xhr, status, error) {
-        // Handle error
-        console.error("Error:", error);
-    });
-
-  updateStatus()
+  playEngineMove();
+  //* update the divs with status
+  updateStatusDivs()
 }
+
+
+function playEngineMove() {
+  console.log("\n\nmaking request, curr time = " + (new Date()))
+
+  //* get best move
+  playEngineMoveGetRequestXHR = $.get("/api/bestmove?fen=" + game.fen(), function (response) {
+    console.log("got response for request, curr time = " + (new Date()))
+    console.log("response: " + response)
+    console.log("\ngame.fen(): " + game.fen())
+
+    //*TODO: parse move properly in case of promotion
+    if (game.move({ from: response[0] + response[1], to: response[2] + response[3] }) === null) {
+      console.log(".move() failed!!!!")
+    }
+    console.log("game.fen(): " + game.fen())
+
+    // update ui board
+    board.position(game.fen())
+  }).fail(function (xhr, status, error) {
+    // Handle error
+    console.error("Error:", error);
+  });
+
+}
+
+
+/*
+ * Call api to get best move
+ * Returns null in case of error
+ */
+function getBestMoveFromFen(fen) {
+  
+}
+
 
 // update the board position after the piece snap
 // for castling, en passant, pawn promotion
@@ -69,46 +116,95 @@ function onSnapEndHandler () {
   board.position(game.fen())
 }
 
-function updateStatus () {
-  var status = ''
+function updateStatusDivs () {
+  var statusString = ''
 
   var moveColor = 'White'
   if (game.turn() === 'b') {
     moveColor = 'Black'
   }
 
-  // checkmate?
   if (game.in_checkmate()) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.'
-  }
+    statusString = 'Game over, ' + moveColor + ' is in checkmate.'
+  } else if (game.in_draw()) {
+    statusString = 'Game over, drawn position'
+  } else {
+    statusString = moveColor + ' to move'
 
-  // draw?
-  else if (game.in_draw()) {
-    status = 'Game over, drawn position'
-  }
-
-  // game still on
-  else {
-    status = moveColor + ' to move'
-
-    // check?
     if (game.in_check()) {
-      status += ', ' + moveColor + ' is in check'
+      statusString += ', ' + moveColor + ' is in check'
     }
   }
 
-  $status.html(status)
-  $fen.html(game.fen())
-  $pgn.html(game.pgn())
+  console.log("statusString= " + statusString)
+  console.log("statusDiv: " + statusDiv)
+  statusDiv.innerHTML = "      " + (statusString)
+  fen.innerHTML = "      " + (game.fen())
+  pgn.innerHTML = "      " + (game.pgn())
 }
 
-var config = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStartHandler,
-  onDrop: onDropHandler,
-  onSnapEnd: onSnapEndHandler
-}
-board = Chessboard('myBoard', config)
 
-updateStatus()
+
+
+
+
+
+
+let currOrientation = 'white'
+
+let newGameWhiteButton = document.getElementById("new-game-white-button")
+let newGameBlackButton = document.getElementById("new-game-black-button")
+
+//let changePlayerButton = document.getElementById("change-player-button");
+//
+//changePlayerButton.addEventListener("click", function(){
+//  currOrientation = (currOrientation === 'white') ? 'black' : 'white';
+//  board.orientation(currOrientation);
+//});
+
+
+
+function reset() {
+  //enable buttons
+
+  game = new Chess()
+
+  var config = {
+    draggable: true,
+    position: 'start',
+    onDragStart: onDragStartHandler,
+    onDrop: onDropHandler,
+    onSnapEnd: onSnapEndHandler,
+
+    showNotation: 'true',
+    showErrors: 'console'
+  }
+  board = Chessboard('myBoard', config)
+
+  updateStatusDivs()
+
+  //if we requested a best move from server, 
+  //then we need to cancel the request
+  if (playEngineMoveGetRequestXHR) {
+    playEngineMoveGetRequestXHR.abort()
+  }
+
+}
+
+
+newGameWhiteButton.addEventListener('click', function() {
+  reset()
+})
+
+
+newGameBlackButton.addEventListener('click', function() {
+  reset()
+  board.orientation('black')
+  playEngineMove()
+})
+
+
+
+
+
+reset()
