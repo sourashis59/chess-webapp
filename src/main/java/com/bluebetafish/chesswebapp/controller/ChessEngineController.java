71a3fb07759f3f7ebe9017c11fe4021994de3021
sourashis-l <sourashiswtf@gmail.com>
2024-12-02 15:53:38 +0000

    Added logger

--- src/main/java/com/bluebetafish/chesswebapp/controller/ChessEngineController.java
+++ src/main/java/com/bluebetafish/chesswebapp/controller/ChessEngineController.java
@@ -1,10 +1,12 @@
 package com.bluebetafish.chesswebapp.controller;
 
 import com.bluebetafish.chesswebapp.service.ChessEngineService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @Service
 @RestController
 @RequestMapping("/api")
@@ -23,7 +25,10 @@ public class ChessEngineController {
         //* Received moveTime is in seconds. Convert it to ms
         moveTime *= 1000;
 
-        return stockFishService.getBestMove(fen, moveTime);
+        log.info("/bestmove handler called: fen: '{}', movetime: {}", fen, moveTime);
+        var bestMove = stockFishService.getBestMove(fen, moveTime);
+        log.info("/bestmove handler: fen: '{}', movetime: {}, bestMove: {}", fen, moveTime, bestMove);
+        return bestMove;
     }
 
 }
